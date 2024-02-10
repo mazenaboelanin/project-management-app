78055f655ed6e82c14b7ac92cea7f81021983a42
mazenaboelanin <mazenaboelanin@gmail.com>
2024-02-10 22:16:18 +0200

    add no project seleted component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from './components/Sidebar/Sidebar';
 import NewProject from './components/NewProject/NewProject';
+import NoProject from './components/NoProject/NoProject';
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      {/* <NewProject /> */}
+      <NoProject />
     </main>
   );
 }
